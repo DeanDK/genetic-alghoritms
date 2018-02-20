851c44852b3388f6d799ada47038807431ceaef5
DeanDK <deanbozic12@gmail.com>
2018-02-20 22:41:22 +0000

    initial commit

---
+++ DNA.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour {
+
+
+	// each float represent gen for color
+	public float r;
+	public float g;
+	public float b;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
