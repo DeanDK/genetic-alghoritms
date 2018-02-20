ab7679266db2256d5c262b714debe558db73bf0f
DeanDK <deanbozic12@gmail.com>
2018-02-20 23:14:06 +0000

    Added population manager script to initialize person prefabs at the start with random colors and positions

--- DNA.cs
+++ DNA.cs
@@ -10,12 +10,28 @@ public class DNA : MonoBehaviour {
 	public float g;
 	public float b;
 
-	// Use this for initialization
-	void Start () {
-		
+	// if person is clicked => dead true
+	bool dead = false;
+	// in order to track how long the person lived
+	public float timeToDie = 0;
+	SpriteRenderer sRenderer;
+	Collider2D sCollider;
+
+	// person is clicked
+	void OnMouseDown(){
+		dead = true;
+		timeToDie = PopulationManager.elapsed;
+		Debug.Log ("Dead At: " + timeToDie);
+		sRenderer.enabled = false;
+		sCollider.enabled = false;
 	}
-	
-	// Update is called once per frame
+		
+	void Start () {
+		sRenderer = GetComponent<SpriteRenderer> ();
+		sCollider = GetComponent<Collider2D> ();
+		sRenderer.color = new Color (r, g, b);
+	}
+
 	void Update () {
 		
 	}
---
+++ PopulationManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour {
+
+	public GameObject personPrefab;
+	public int populationSize = 10;
+	List<GameObject> population = new List<GameObject>();
+	public static float elapsed = 0;
+	int trialTime = 15;
+	int generation = 1;
+
+
+	GUIStyle guiStyle = new GUIStyle();
+	void OnGui(){
+		guiStyle.fontSize = 50;
+		guiStyle.normal.textColor = Color.white;
+		GUI.Label (new Rect (10, 10, 100, 20), "Generation: " + generation, guiStyle);
+		GUI.Label (new Rect (10, 65, 100, 20), "Trial Time: " + (int)elapsed, guiStyle);
+	}
+
+	// initialize population and set gen colors
+	void Start () {
+		for (int i = 0; i < populationSize; i++) {
+			Vector3 pos = new Vector3 (Random.Range (-8.9f, 8.9f), Random.Range (-3.9f, 3.9f), 0);
+			GameObject go = Instantiate (personPrefab, pos, Quaternion.identity); // Quaternion gives 0 rotational value
+			go.GetComponent<DNA>().r = Random.Range(0.0f, 1.0f);
+			go.GetComponent<DNA>().g = Random.Range(0.0f, 1.0f);
+			go.GetComponent<DNA>().b = Random.Range(0.0f, 1.0f);
+			population.Add (go);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsed += Time.deltaTime;
+		if (elapsed > trialTime) {
+			BreedNewPopulation ();
+			elapsed = 0;
+		}
+	}
+}
